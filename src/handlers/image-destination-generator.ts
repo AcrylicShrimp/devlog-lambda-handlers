1b01572361148222a0693e949b318928562534d4
AcrylicShrimp <led789zxpp@naver.com>
2021-05-31 21:41:36 +0900

    wip

--- src/handlers/image-destination-generator.ts
+++ src/handlers/image-destination-generator.ts
@@ -3,25 +3,31 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { randomBytes } from 'crypto';
 
+const badRequestRes = {
+  statusCode: 400,
+  body: JSON.stringify({
+    message: 'body should be a valid json that contains post-id and title properties',
+  }),
+};
+
 export const handler: APIGatewayProxyHandler = async (event) => {
   try {
-    if ((event.headers['content-type'] ?? '') !== 'text/plain')
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: 'body should be non-empty text/plain',
-        }),
-      };
+    if ((event.headers['content-type'] ?? '') !== 'application/plain') return badRequestRes;
 
-    const title = event.body?.trim();
+    if (!event.body) return badRequestRes;
 
-    if (!title)
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          message: 'body should be non-empty text/plain',
-        }),
-      };
+    let body;
+
+    try {
+      body = JSON.parse(event.body);
+    } catch {
+      return badRequestRes;
+    }
+
+    const postId = body['post-id']?.trim();
+    const title = body.title?.trim();
+
+    if (typeof postId !== 'string' || !postId || typeof title !== 'string' || !title) return badRequestRes;
 
     const token = await new Promise<string>((resolve, reject) =>
       randomBytes(32, (err, buf) => (err ? reject(err) : resolve(buf.toString('hex')))),
@@ -34,10 +40,12 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       Key: `${process.env.S3_BUCKET_KEY_PREFIX_IMAGE!}${token}`,
       Conditions: [
         {
+          'x-amz-meta-post-id': postId,
           'x-amz-meta-title': title,
         },
       ],
       Fields: {
+        'x-amz-meta-post-id': postId,
         'x-amz-meta-title': title,
       },
     });
