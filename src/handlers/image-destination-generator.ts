589c91b011cb907b62f834968ecd6e248a6aa1d8
AcrylicShrimp <led789zxpp@naver.com>
2021-06-29 21:54:35 +0900

    feat: remove unneeded try-catch.

--- src/handlers/image-destination-generator.ts
+++ src/handlers/image-destination-generator.ts
@@ -11,81 +11,73 @@ const badRequestRes = {
 };
 
 export const handler: APIGatewayProxyHandler = async (event) => {
+  if (!event.headers || event.headers['content-type'] !== 'application/json' || !event.body) return badRequestRes;
+
+  let body;
+
   try {
-    if (!event.headers || event.headers['content-type'] !== 'application/json' || !event.body) return badRequestRes;
+    body = JSON.parse(event.body);
+  } catch {
+    return badRequestRes;
+  }
 
-    let body;
+  const postUUID = body['post-uuid']?.trim();
+  const title = body.title?.trim();
 
-    try {
-      body = JSON.parse(event.body);
-    } catch {
-      return badRequestRes;
-    }
+  if (typeof postUUID !== 'string' || !postUUID || typeof title !== 'string' || !title) return badRequestRes;
 
-    const postUUID = body['post-uuid']?.trim();
-    const title = body.title?.trim();
+  const req = https.request({
+    host: process.env.BACKEND_IMAGE_HOST!,
+    method: 'POST',
+    path: `${process.env.BACKEND_IMAGE_PATH_PREFIX!}/${postUUID}/images`,
+    timeout: 5000,
+    headers: {
+      Accept: 'application/json',
+      'Accept-Charset': 'utf-8',
+    },
+  });
+  const res = await new Promise<string>((resolve, reject) => {
+    req.on('error', reject);
+    req.on('response', (res) => {
+      if (res.statusCode !== 201) return reject(`the server returned: ${res.statusCode} ${res.statusMessage}`);
 
-    if (typeof postUUID !== 'string' || !postUUID || typeof title !== 'string' || !title) return badRequestRes;
+      const chunks: Buffer[] = [];
 
-    const req = https.request({
-      host: process.env.BACKEND_IMAGE_HOST!,
-      method: 'POST',
-      path: `${process.env.BACKEND_IMAGE_PATH_PREFIX!}/${postUUID}/images`,
-      timeout: 5000,
-      headers: {
-        Accept: 'application/json',
-        'Accept-Charset': 'utf-8',
-      },
+      res.on('error', reject);
+      res.on('data', (chunk) => chunks.push(chunk));
+      res.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')));
     });
-    const res = await new Promise<string>((resolve, reject) => {
-      req.on('error', reject);
-      req.on('response', (res) => {
-        if (res.statusCode !== 201) return reject(`the server returned: ${res.statusCode} ${res.statusMessage}`);
+    req.end();
+  });
+  const imageUUID = JSON.parse(res).uuid;
 
-        const chunks: Buffer[] = [];
-
-        res.on('error', reject);
-        res.on('data', (chunk) => chunks.push(chunk));
-        res.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')));
-      });
-      req.end();
-    });
-    const imageUUID = JSON.parse(res).uuid;
-
-    const s3 = new S3Client({
-      region: process.env.AWS_S3_REGION!,
-    });
-    const result = await createPresignedPost(s3, {
-      Bucket: process.env.AWS_S3_BUCKET!,
-      Key: `${process.env.S3_BUCKET_KEY_PREFIX!}${postUUID}/images/${imageUUID}`,
-      Conditions: [
-        {
-          'x-amz-meta-post-uuid': postUUID,
-        },
-        {
-          'x-amz-meta-image-uuid': imageUUID,
-        },
-        {
-          'x-amz-meta-title': title,
-        },
-        ['starts-with', '$Content-Type', 'image/'],
-      ],
-      Fields: {
+  const s3 = new S3Client({
+    region: process.env.AWS_S3_REGION!,
+  });
+  const result = await createPresignedPost(s3, {
+    Bucket: process.env.AWS_S3_BUCKET!,
+    Key: `${process.env.S3_BUCKET_KEY_PREFIX!}${postUUID}/images/${imageUUID}`,
+    Conditions: [
+      {
         'x-amz-meta-post-uuid': postUUID,
+      },
+      {
         'x-amz-meta-image-uuid': imageUUID,
+      },
+      {
         'x-amz-meta-title': title,
       },
-    });
+      ['starts-with', '$Content-Type', 'image/'],
+    ],
+    Fields: {
+      'x-amz-meta-post-uuid': postUUID,
+      'x-amz-meta-image-uuid': imageUUID,
+      'x-amz-meta-title': title,
+    },
+  });
 
-    return {
-      statusCode: 201,
-      body: JSON.stringify(result),
-    };
-  } catch (err) {
-    console.error(`An error occurred: ${err}`);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'internal server error' }),
-    };
-  }
+  return {
+    statusCode: 201,
+    body: JSON.stringify(result),
+  };
 };
