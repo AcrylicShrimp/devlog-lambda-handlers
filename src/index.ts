6a1f4499d1d1e0f5f2475f6b398f18978a450791
AcrylicShrimp <led789zxpp@naver.com>
2021-06-30 09:56:14 +0900

    fix: add a missing handler.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { handler as imageDestinationGenerator } from './handlers/image-destination-generator';
 export { handler as imageHandler } from './handlers/image-handler';
+export { handler as thumbnailDestinationGenerator } from './handlers/thumbnail-destination-generator';
 export { handler as thumbnailHandler } from './handlers/thumbnail-handler';
 export { handler as uploadEventHandler } from './handlers/upload-event-handler';
